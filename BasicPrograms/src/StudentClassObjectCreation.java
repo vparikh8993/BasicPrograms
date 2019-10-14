986ad695fc7440699c2a89e062279cbadb8d0420
Vishwa Parikh <parikhvishwa92@gmail.com>
2019-10-14 12:47:04 -0500

    object cretion

--- BasicPrograms/src/StudentClassObjectCreation.java
+++ BasicPrograms/src/StudentClassObjectCreation.java
@@ -1,2 +1,14 @@
 public class StudentClassObjectCreation {
+
+    int id=45;
+    String name="vishwa";
+
+    public static void main(String[] args) {
+        //creating object of class
+        StudentClassObjectCreation s=new StudentClassObjectCreation();
+        System.out.println("Hello this is my first program to learn java");
+        //calling instance variable defined in the class by oject created inside the method
+        System.out.println(s.id);
+        System.out.println(s.name);
+    }
 }
